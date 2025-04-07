529d399da62c1e741b3a47d9a35388014541fdbd
achalp1p1 <achal.b.bansal@avanade.com>
2025-04-07 13:29:40 +0800

    Simplify Azure deployment configuration and update dependencies

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
-import os
 from src.app import app
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port) 
+# This is the WSGI entry point
+application = app
+
+if __name__ == '__main__':
+    app.run() 
